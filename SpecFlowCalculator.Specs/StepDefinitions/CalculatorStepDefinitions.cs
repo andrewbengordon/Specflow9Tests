982aa2d31f5f0ee33a1b0c9c4fc44c41a1242ccb
andrewbengordon <andrewbengordon@gmail.com>
2022-02-23 21:12:44 -0700

    Add project files.

---
+++ SpecFlowCalculator.Specs/StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,64 @@
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowCalculator.Specs.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        private readonly Calculator _calculator = new Calculator();
+        private int _result;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            _calculator.FirstNumber = number;
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            _calculator.SecondNumber = number;
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            _result = _calculator.Add();
+        }
+
+        [When("the two numbers are subtracted")]
+        public void WhenTheTwoNumbersAreSubtracted()
+        {
+            _result = _calculator.Subtract();
+        }
+
+        [When("the two numbers are multiplied")]
+        public void WhenTheTwoNumbersAreMultiplied()
+        {
+            _result = _calculator.Multiply();
+        }
+
+        [When("operation (.*) is done to the number (.*)")]
+        public void WhenOperationIsDoneToTheNumber(string p0, int p1)
+        {
+            _result = _calculator.Operator(p0, p1);
+        }
+
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            _result.Should().Be(result);
+        }
+    }
+}
---
+++ Specflow9Tests/Calculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SpecFlowCalculator
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+
+        public int Subtract()
+        {
+            return FirstNumber - SecondNumber;
+        }
+
+        public int Multiply()
+        {
+            return FirstNumber * SecondNumber;
+        }
+
+        public int Divide()
+        {
+            return FirstNumber / SecondNumber;
+        }
+
+        public int Operator(string p0, int p1)
+        {
+            switch (p0)
+            {
+                case "+":
+                    return FirstNumber += p1;
+                case "-":
+                    return FirstNumber -= p1;
+                case "*":
+                    return FirstNumber *= p1;
+                case "/":
+                    return FirstNumber /= p1;
+                case "%":
+                    return FirstNumber %= p1;
+                default:
+                    return FirstNumber;
+
+            }
+        }
+    }
+}
